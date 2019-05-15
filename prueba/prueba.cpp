63b6eadedbdebc753f7e928777c9c51fdbb7bb68
Genesis Valdivia <danielvaldiviaramos@hotmail.com>
2019-05-15 10:52:42 -0500

    prueba

---
+++ prueba/prueba.cpp
@@ -0,0 +1,117 @@
+
+#include <iostream>
+using namespace std;
+
+template<class T>
+struct DinamicArray{
+	T *arr;
+	int size;
+	DinamicArray(){
+		size=0;
+		arr=new T[size];
+	}	
+	
+	void resize(int newsize){
+		int minsize=size>newsize ? newsize : size;
+		T *temp=new T[newsize];
+		for(int i=0;i<minsize;i++)
+			temp[i]=arr[i];
+		size=newsize;
+		delete[] arr;
+		arr=temp;
+	}
+	
+	void push_back(T element){
+		resize(size+1);
+		arr[size-1]=element;
+	}
+	
+	void clear(){
+		resize(0);
+	}
+	
+	int getSize()const{
+		return size;
+	}
+	~DinamicArray(){
+		delete[] arr;
+	}
+};
+
+
+class Editor{
+	private:
+		int cuadrante;
+		bool **matrix;
+
+	public:
+		DinamicArray<float> vertices;
+		DinamicArray<unsigned int> indices;
+		Editor();
+		void draw();
+		void addElement();
+		~Editor();
+	
+};
+
+Editor::Editor(){
+	int cuadrante=2;
+	matrix=new bool*[80];
+	for(int i=0;i<80;i++){
+		*(matrix+i)=new bool[80];
+	}
+	for(int i=0;i<80;i++)
+		for(int j=0;j<80;j++)
+			*(*(matrix+i)+j)=true;
+			
+	for(int i=0;i<80;i++){
+		for(int j=0;j<80;j++){
+			vertices.push_back(float(j*cuadrante));
+			vertices.push_back(float(i*cuadrante));
+			vertices.push_back(0.0f);
+		}
+	}
+	
+	float cont=0.0f;
+	for(int i=0;i<79;i++){
+		for(int j=0;j<79;j++){
+			indices.push_back(cont);
+			indices.push_back(cont+1);
+			indices.push_back(cont+81);
+			indices.push_back(cont);
+			indices.push_back(cont+80);
+			indices.push_back(cont+81);	
+			cont++;
+		}
+	}
+}
+
+
+void Editor::draw(){
+	for(int i=0;i<vertices.getSize();i++){
+		if(!(i%3)){
+			cout<<endl;
+		}
+			
+		cout<<vertices.arr[i]<<" ";
+	}
+}
+
+
+
+Editor::~Editor(){
+	for(int i=0;i<80;i++){
+		delete[] *(matrix+i);
+	}
+	delete[] matrix;
+}
+
+int main(){
+	Editor editor;
+//	cout<<editor.vertices.getSize();
+	/*int a=500;
+	float b=float(a);
+	cout<<b;*/
+	cout<<editor.indices.getSize();
+	//editor.draw();
+}
